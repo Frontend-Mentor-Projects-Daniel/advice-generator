32031abb1b2ffa4b6d34ed96b36e7d6ee1eabb8b
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-08-29 13:07:42 -0400

    create functionality for advice generator

--- develop/components/AdviceGenerator/AdviceGenerator.js
+++ develop/components/AdviceGenerator/AdviceGenerator.js
@@ -16,6 +16,33 @@ function define(html) {
     // VARIABLES
 
     // METHODS
+    handleClick() {
+      fetch('https://api.adviceslip.com/advice', {
+        cache: 'no-cache',
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const {
+            slip: { id, advice },
+          } = data;
+
+          const cardQuote =
+            this.shadowRoot.children[2].children[0].children[1].children[0];
+          cardQuote.textContent = advice;
+
+          const cardTitle = this.shadowRoot.children[2].children[0].children[0];
+          cardTitle.textContent = `Advice #${id}`;
+        })
+        .catch((e) => {
+          if (e) throw new Error(e.message);
+        });
+    }
+
+    // LIFE CYCLE METHODS
+    connectedCallback() {
+      const generateAdviceBtn = this.shadowRoot.children[2].children[2];
+      generateAdviceBtn.addEventListener('click', () => this.handleClick());
+    }
   }
   window.customElements.define('advice-generator', AdviceGenerator);
 }
